60d105e3b2f22f42e34b23fd9efc2641a1f9de79
mario dibella <mariodibella4@gmail.com>
2021-02-16 13:54:29 -0800

    adding objects

--- src/MainFrameController.java
+++ src/MainFrameController.java
@@ -2,12 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+//current bugs each action produces a new JFrame I'm sure this is an architecture issue
 public class MainFrameController extends JFrame {
-
+    public final String HOME_PAGE = "home page";
+    public final String GUEST_REG_PAGE = "guest reg page";
+    public final String GUEST_LOGIN_PAGE = "guest login page";
     private GuestLoginForm guestLoginForm;
     private GuestRegistrationForm guestRegistrationForm;
-    private MainFrameController container;
+    private final CardLayout cardLayout;
+    private JPanel mainPane;
     public MainFrameController(){
         super("Demo I");
         setSize(600,600);
@@ -15,7 +18,23 @@ public class MainFrameController extends JFrame {
         setVisible(true);
         //above sets the stage below is an example how to manifest the form with in the original frame.
         //technically its not a Form but a JPanel
+         mainPane = new JPanel();
+        mainPane.setPreferredSize(new Dimension(500,500));
+        cardLayout=new CardLayout();
+        mainPane.setLayout(cardLayout);
+        guestRegistrationForm =new GuestRegistrationForm();
+        guestLoginForm =new GuestLoginForm();
+        mainPane.add(HOME_PAGE,new JPanel());
+        mainPane.add(GUEST_REG_PAGE,guestRegistrationForm);
+        mainPane.add(GUEST_LOGIN_PAGE,guestLoginForm);
+
+        mainPane.setVisible(true);
+        setLayout(new BorderLayout());
+        add(mainPane,BorderLayout.CENTER);
+        //pack();
         setJMenuBar(createMenuBar());
+
+
 
     }
     private JMenuBar createMenuBar(){
@@ -27,24 +46,9 @@ public class MainFrameController extends JFrame {
         guestMenu.add(guestLogin);
         guestMenu.addSeparator();
         guestMenu.add(guestReg);
-        guestReg.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                container=new MainFrameController();
-                guestRegistrationForm =new GuestRegistrationForm();
-                container.getContentPane().add(guestRegistrationForm);
-                guestRegistrationForm.setVisible(true);
-            }
-        });
-        guestLogin.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                container=new MainFrameController();
-                guestLoginForm =new GuestLoginForm();
-                container.getContentPane().add(guestLoginForm);
-                guestLoginForm.setVisible(true);
-            }
-        });
+        guestReg.addActionListener(e ->cardLayout.show(mainPane,GUEST_REG_PAGE));
+        guestLogin.addActionListener(e -> cardLayout.show(mainPane,GUEST_LOGIN_PAGE));
+
 
 
 
